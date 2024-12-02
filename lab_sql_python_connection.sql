c923e3d013b30f88b0136e489de2de2705670ea0
Edu-Martinez-91 <eduardomarman91@gmail.com>
2024-12-02 17:09:18 +0100

    Lab Solved

--- lab_sql_python_connection.sql
+++ lab_sql_python_connection.sql
@@ -9,9 +9,9 @@ WHERE YEAR(rental_date)= 2005 AND MONTH(rental_date)=5;
 -- The function should also include the month and year as parameters and use them to name the new column according to the month and year, 
 -- for example, if the input month is 05 and the year is 2005, the column name should be "rentals_05_2005".
 
-SELECT 
-    COUNT(r.rental_id) AS rentals_05_2005, 
-    CONCAT(c.first_name, ' ', c.last_name) AS customer_name
+SELECT
+	CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    COUNT(r.rental_id) AS rentals_05_2005
 FROM rental r
 JOIN customer c USING (customer_id)
 WHERE YEAR(r.rental_date) = 2005 AND MONTH(r.rental_date) = 5
@@ -23,4 +23,5 @@ ORDER BY rentals_05_2005 DESC;
 -- DataFrames as input containing the number of rentals made by each customer in different months and years. 
 -- The function should return a combined DataFrame with a new 'difference' column, which is the difference 
 -- between the number of rentals in the two months.
+
 
